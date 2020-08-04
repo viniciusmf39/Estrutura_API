1edbd90d0136b62c22473e98834d88b7c598529a
vinicius_mf <vinimarfer@gmail.com>
2020-08-04 14:53:05 -0300

    no message

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,9 @@ import cors from 'cors';
 import UserController from './app/controllers/UserController';
 
 const routes = Router();
-
-routes.get('/', (req, res) => res.json({ result: 'TEST-API' }));
 routes.use(cors());
+routes.get('/', (req, res) => res.json({ result: 'TEST-API' }));
+
 
 routes.post('/users', UserController.store);
 routes.get('/users', UserController.index);
